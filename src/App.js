40ecc6259b8ec6caed766f69ae47f98be8100265
Archit Suthar <suthararchit8@gmail.com>
2022-01-29 19:34:28 +0530

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import AddBug from './components/Bug/AddBug';
+import { Table } from 'react-bootstrap';
 function App() {
   return (
     <div className="App">
