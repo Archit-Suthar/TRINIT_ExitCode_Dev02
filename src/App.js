f74b8eb82305ebc6b05b1e7167baa097153c8ce1
Om Tita <omtita971@gmail.com>
2022-01-29 18:40:34 +0530

    added buglist table

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import Ournav from './components/navbar/navbar';
-
+import Table from './components/buglist/buglist'
 function App() {
   return (
     <div className="App">
       <Ournav/>
+      <Table/>
     </div>
   );
 }
