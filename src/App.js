b28d4b2ee971545b89eac855230c8de3482e6956
Archit Suthar <suthararchit8@gmail.com>
2022-01-29 19:33:37 +0530

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
 import AddBug from './components/Bug/AddBug';
-
-import Ournav from './components/navbar/navbar';
-import Table from './components/buglist/buglist'
 function App() {
   return (
     <div className="App">
